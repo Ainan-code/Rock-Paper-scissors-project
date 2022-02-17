6446220685dc77ad7f6455f78a5e2ef8a17c4796
AINAN <ainanhassan123@gmail.com>
2022-02-17 23:08:29 +0300

    created the first function for the computer playing randomly

---
+++ script.js
@@ -0,0 +1,8 @@
+ var gameoutput = ["Rock","Paper","Scissors"];
+
+ var random = gameoutput[Math.floor(Math.random()*gameoutput.length)];
+
+function Computerplay() {
+    // function for the random computer guess
+    return random;
+} 
