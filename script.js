59ba5acd43591099109af9afa50873e39cc47caa
AINAN <ainanhassan123@gmail.com>
2022-02-19 17:59:06 +0300

    index.html

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
- var gameoutput = ["Rock","Paper","Scissors"];
+ var gameoutput = ["rock","paper","scissors"];
 
  var random = gameoutput[Math.floor(Math.random()*gameoutput.length)];
 
@@ -6,3 +6,42 @@ function Computerplay() {
     // function for the random computer guess
     return random;
 } 
+
+let playerselection;
+let computerselection ;
+
+
+
+function winner(playerselection, computerselection) {
+   playerselection = window.prompt("make your move").toLowerCase();
+   computerselection = Computerplay();
+       let result = 0;
+   if (playerselection==("rock") && computerselection==("paper"))  
+      return result=("You lose! Paper beats Rock"); 
+   
+   
+   if (playerselection==("rock") && computerselection==("scissors")) 
+      return result=("You win! Rock beats scissors");
+   
+
+   if (playerselection==("paper") && computerselection==("scissors")) 
+      return result=("You lose! scissors beats paper");
+
+   if (playerselection==("paper") && computerselection==("rock")) 
+      return result=("You win! Paper beats Rock");
+
+   if (playerselection==("scissors") && computerselection==("rock")) 
+      return result=("You lose! Rock beats scissors");
+
+   if (playerselection==("scissors") && computerselection==("paper")) 
+      return result=("You win! scissors beats paper");
+
+      if (playerselection===computerselection) 
+      return result=("its a tie");
+       
+   console.log(result)
+      
+}
+
+   
+  
